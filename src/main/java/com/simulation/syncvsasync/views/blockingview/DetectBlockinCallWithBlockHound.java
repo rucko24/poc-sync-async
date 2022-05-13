67e8a26db2a8d0d98ac8eec3470ba4afe1788ca0
rucko24 <rucko24@gmail.com>
2022-05-13 22:57:57 +0200

    Refactor view with BlockHound

--- src/main/java/com/simulation/syncvsasync/views/blockingview/DetectBlockinCallWithBlockHound.java
+++ src/main/java/com/simulation/syncvsasync/views/blockingview/DetectBlockinCallWithBlockHound.java
@@ -3,14 +3,15 @@ package com.simulation.syncvsasync.views.blockingview;
 import com.simulation.syncvsasync.enumsizesfornumbers.EnumSizeForRandomNumbers;
 import com.simulation.syncvsasync.service.ReactiveRandomNumbers;
 import com.simulation.syncvsasync.service.SyncRandomNumbers;
+import com.simulation.syncvsasync.util.NotificationsUtils;
 import com.simulation.syncvsasync.views.main.MainView;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.SpringComponent;
@@ -22,9 +23,12 @@ import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Log4j2
 @SpringComponent
@@ -32,16 +36,18 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Route(value = "blocking-calls", layout = MainView.class)
 @PageTitle("BlockHound")
 @RequiredArgsConstructor
-public class DetectBlockinCallWithBlockHound extends VerticalLayout {
+public class DetectBlockinCallWithBlockHound extends VerticalLayout implements NotificationsUtils {
     //
-    private final ReactiveRandomNumbers service;
+    private final ReactiveRandomNumbers reactiveRandomNumbers;
     private final SyncRandomNumbers syncRandomNumbers;
+    private final TextArea textArea = new TextArea();
     private final ComboBox<Scheduler> schedulersComboBox = new ComboBox<>("Make call with a Scheduler");
 
     @PostConstruct
     public void init() {
+        this.setSizeFull();
         this.fillComboBoxWithSchedulersAndCustomize();
-        super.add(schedulersComboBox);
+        super.add(schedulersComboBox, this.textArea);
     }
 
     private void fillComboBoxWithSchedulersAndCustomize() {
@@ -53,13 +59,18 @@ public class DetectBlockinCallWithBlockHound extends VerticalLayout {
         schedulersComboBox.setItems(schedulersList);
         schedulersComboBox.setItemLabelGenerator(item -> {
             if (item.equals(Schedulers.immediate())) {
-                return "Schedulers.immediate()";
+                return "Schedulers.immediate() 😁";
+            } else if (item.equals(Schedulers.boundedElastic())) {
+                return "Schedulers.boundedElastic() 😁";
             }
-            return item.toString();
+            return item.toString().concat(" 😭");
         });
         this.schedulersComboBox.setClearButtonVisible(true);
         this.schedulersComboBox.setPlaceholder("Select Scheduler...");
         this.schedulersComboBox.setWidth("50%");
+        textArea.setValueChangeMode(ValueChangeMode.EAGER);
+        this.textArea.setSizeFull();
+        super.expand(this.textArea);
     }
 
     /**
@@ -71,23 +82,31 @@ public class DetectBlockinCallWithBlockHound extends VerticalLayout {
         this.schedulersComboBox.addValueChangeListener(value -> {
             if (Objects.nonNull(value)) {
                 Mono.fromCallable(() ->
-                                Mono.just(this.syncRandomNumbers.syncFrencuency(EnumSizeForRandomNumbers.TEN_MILLION.getSize())))
-                        .subscribeOn(value.getValue())
-                        //Detected error with BlockHound
-                        .onErrorContinue((Throwable error, Object o) -> {
-                            log.error("Error -> {} {}", value.getValue(), error);
-                            ui.access(() -> {
-                                final Notification n = new Notification(value.getValue() + " " + error);
-                                n.addThemeVariants(NotificationVariant.LUMO_ERROR);
-                                n.setDuration(2500);
-                                n.open();
-                            });
-                        })
+                                this.reactiveRandomNumbers
+                                        .monoWithBlockingCallInside(EnumSizeForRandomNumbers.TEN_MILLION.getSize()))
                         //Set a Scheduler at runtime
+                        .subscribeOn(value.getValue())
+                        .flatMap(Function.identity())
+                        //Detected error with BlockHound
+                        .doOnError(error -> {
+                            ui.access(() -> {
+                                final Scheduler scheduler = value.getValue();
+                                this.showError(scheduler+" "+ error);
+                                textArea.clear();
+                                String stackTrace = scheduler.toString()
+                                        .concat(" ")
+                                        .concat(error.toString())
+                                        .concat("\n\n")
+                                        .concat(Arrays.stream(error.getStackTrace())
+                                        .map(String::valueOf)
+                                        .collect(Collectors.joining("\n")));
+                                textArea.setValue(stackTrace);
+                            });
+
+                        })
                         .doOnEach(signal -> log.info("Thread name: {}", Thread.currentThread().getName()))
-                        .flatMap(mapMono -> Mono.defer(() -> mapMono))
                         .subscribe(monoMap -> {
-                            ui.access(() -> Notification.show("Map: " + monoMap));
+                            ui.access(() -> this.showMessage("Map: " + monoMap));
                         });
 
             }
