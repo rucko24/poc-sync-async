0f3653eef9af5ad921ea67dcdaef31634bb9e9f6
rucko24 <rucko24@gmail.com>
2022-05-13 22:56:42 +0200

    Add constants

---
+++ src/main/java/com/simulation/syncvsasync/util/DemoConstants.java
@@ -0,0 +1,14 @@
+package com.simulation.syncvsasync.util;
+
+/**
+ * @author rubn
+ */
+public class DemoConstants {
+
+    public static final String RESULT_MAP = "Result map: {}";
+    public static final String DAEMON = "Daemon: {}";
+    public static final String THREAD_NAME = "Thread name: {}";
+    public static final String THREADGROUP = "ThreadGroup: {}";
+    public static final String THREAD_STATE = "Thread state: {}";
+
+}
