c3e11fe908afe402588c8d1342e9c31934ed7593
rucko24 <rucko24@gmail.com>
2023-12-06 13:43:04 +0100

    updated AboutView

--- src/main/java/com/simulation/syncvsasync/views/about/AboutView.java
+++ src/main/java/com/simulation/syncvsasync/views/about/AboutView.java
@@ -10,7 +10,13 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.shared.Tooltip;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.vaadin.flow.theme.lumo.LumoUtility.AlignItems;
+import com.vaadin.flow.theme.lumo.LumoUtility.Display;
+import com.vaadin.flow.theme.lumo.LumoUtility.FlexDirection;
+import com.vaadin.flow.theme.lumo.LumoUtility.JustifyContent;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Top;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainView.class)
@@ -35,7 +41,7 @@ public class AboutView extends VerticalLayout {
 
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+
     }
 
     private Div sourceOfThePoc() {
@@ -45,44 +51,34 @@ public class AboutView extends VerticalLayout {
         final Div div = new Div();
         div.add(anchorLoom);
         Tooltip.forComponent(div).setText("https://github.com/rucko24/poc-sync-asyn");
-        div.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        div.addClassNames(Top.MEDIUM, Margin.Bottom.MEDIUM);
         return div;
     }
 
     private Div row() {
         final Div row = new Div();
-        row.addClassName("row-about");
-        row.getStyle().set("display", "flex");
-        row.getStyle().set("align-items", "baseline");
-        row.getStyle().set("justify-content", "center");
-        row.getStyle().set("gap", "5px");
-
+        row.setWidthFull();
+        row.addClassNames(Display.FLEX, FlexDirection.COLUMN, AlignItems.CENTER, JustifyContent.CENTER);
         final Div div = new Div();
         div.getElement().setProperty(INNER_HTML, "<p>This project internally uses </p>");
         row.add(div);
-        row.add(this.projectLoomDiv());
-        row.add(new Span(" and "));
-        row.add(this.blockHoundDiv());
+        row.add(this.projectLoomAndBlockHoundDiv());
         return row;
     }
 
-    private Div projectLoomDiv() {
+    private Div projectLoomAndBlockHoundDiv() {
         final Anchor anchorLoom = new Anchor(LOOM_URL);
         anchorLoom.getElement().setProperty(INNER_HTML, "<strong>Project Loom</strong>");
         anchorLoom.setTarget(TARGET_BLANK);
-        final Div div = new Div();
-        div.add(anchorLoom);
-        Tooltip.forComponent(div).setText(LOOM_URL);
-        return div;
-    }
 
-    private Div blockHoundDiv() {
         final Anchor blockHoundAnchor = new Anchor("https://github.com/reactor/BlockHound");
         Tooltip.forComponent(blockHoundAnchor).setText("https://github.com/reactor/BlockHound");
         blockHoundAnchor.getElement().setProperty(INNER_HTML, "<strong>BlockHound 🔥</strong>");
         blockHoundAnchor.setTarget(TARGET_BLANK);
+
         final Div div = new Div();
-        div.add(blockHoundAnchor);
+        div.add(anchorLoom, new Span(" and "), blockHoundAnchor);
+        Tooltip.forComponent(div).setText(LOOM_URL);
         return div;
     }
 
